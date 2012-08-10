2896a1e83363824952282fbab7d6b44d948aed33
Jack Foy <jfoy@whitepages.com>
2012-08-09 21:53:29 -0700

    Initial commit, with working surveyor config

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SurveyorProto::Application.config.secret_token = '2b8bde967b68e44a9eb0472c4821948510865370fa11744062dfc89babe664dd159596dac17ff32f7523b3fa98c02cf19e27261f41191917b36a62bf2c1fb58a'
